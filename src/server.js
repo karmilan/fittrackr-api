c8b2668d2e6cbf3a997daa2ecd9630915e12d303
karmilan <delftkarmilan@gmail.com>
2026-02-04 00:48:37 +0530

    db con

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/fittrackr'
 
 const startServer = async () => {
     try {
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
 
         app.listen(PORT, () => {
